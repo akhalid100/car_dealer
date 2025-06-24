f41dcde706d5fcea3e0778edcd2bd9b030e52a9e
akhalid100 <dev-akhalid100@hotmail.com>
2025-06-24 22:20:09 +0200

    template setup

--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'pages/home.html')
+
+# about
+def about(request):
+    return render(request,'pages/about.html')
+
+# services
+def services(request):
+    return render(request,'pages/services.html')
+
+# contact
+def contact(request):
+    return render(request,'pages/contact.html')
